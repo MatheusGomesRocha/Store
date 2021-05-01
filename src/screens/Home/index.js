2f284a9e11f71848c37878c57a0a57710da6c121
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-01 11:22:31 -0300

    Adicionando imagem de um Relogio para usar na area de Promotion na Home + finalizando PromotionArea

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+import WatchPng from '../../assets/img/watch.png';
 import SearchIcon from '../../assets/svg/search.svg';
 import NotificationIcon from '../../assets/svg/bell.svg';
 import {
@@ -9,10 +9,15 @@ import {
     Header,
     DivInputSearch,
     Input,
-    ButtonNotification
+    ButtonNotification,
+
+    PromotionArea,
+    PromotionInsideLeft,
+    PromotionText,
+    PromotionImg
 } from './styles';
 
-import { gray200, gray500, gray800 } from '../../styles';
+import { gray200, gray800 } from '../../styles';
 
 export default function Home() {
     return(
@@ -20,7 +25,7 @@ export default function Home() {
 
             <Header>
                 <DivInputSearch>
-                    <SearchIcon fill={gray500} width="25px" height="25px" />
+                    <SearchIcon fill={gray200} width="25px" height="25px" />
                     <Input placeholderTextColor={gray200} placeholder="Search" />
                 </DivInputSearch>
 
@@ -28,6 +33,17 @@ export default function Home() {
                     <NotificationIcon fill={gray800} width="25px" height="25px" />
                 </ButtonNotification>
             </Header>
+
+            <PromotionArea>
+                <PromotionInsideLeft>
+                    <PromotionText>Hot Sale</PromotionText>
+                    <PromotionText style={{fontWeight: 'bold'}}>40% Off</PromotionText>
+                    <PromotionText style={{marginTop: 25, fontSize: 20}}>Rolex Smartwatch</PromotionText>
+                </PromotionInsideLeft>
+
+                <PromotionImg source={require('../../assets/img/watch.png')} />
+            </PromotionArea>
+
         </HomeContainer>
     )
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,18 +1,19 @@
 import styled from 'styled-components/native';
-import { white, gray100, gray200, gray500, gray800 } from '../../styles';
+import { white, gray50, gray800, defaultColor } from '../../styles';
 
 export const HomeContainer = styled.SafeAreaView`
     flex: 1;
     background: ${white};
 `;
+
 export const Header = styled.View`
     flex-direction: row;
     align-items: center;
-    margin: 20px 15px;
+    margin: 20px 15px 10px 15px;
 `;
 export const DivInputSearch = styled.View`
     flex-direction: row;
-    background: ${gray100};
+    background: ${gray50};
     border-radius: 25px;
     height: 56px;
     padding-left: 15px;
@@ -25,11 +26,34 @@ export const Input = styled.TextInput`
     color: ${gray800};
 `;
 export const ButtonNotification = styled.TouchableOpacity`
-    background: ${gray100};
+    background: ${gray50};
     height: 56px;
     width: 56px;
     border-radius: 28px;
     margin-left: 15px;
     align-items: center;
     justify-content: center;
+`;
+
+export const PromotionArea = styled.View`
+    margin: 10px 15px;
+    background: ${defaultColor};
+    border-radius: 10px;
+    height: 180px;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0 5px 0 15px;
+`;
+export const PromotionInsideLeft = styled.View`
+    margin: 20px 0;
+    width: 55%;
+`;
+export const PromotionText = styled.Text`
+    color: ${white};
+    font-size: 25px;
+    margin-top: 1px;
+`;
+export const PromotionImg = styled.Image`
+    height: 180px;
+    width: 180px;
 `;
