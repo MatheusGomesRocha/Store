a9a306caeabcbfd862771a105a80c9e52d722838
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-03 08:37:04 -0300

    Montando estrutura da tela Product + adicionando duas novas imagens para usar nessa tela

--- src/screens/Product/index.js
+++ src/screens/Product/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import FavoriteIcon from '../../assets/svg/heart.svg';
+import CartIcon from '../../assets/svg/shopping-basket.svg';
+{/* <StatusBar translucent={true} backgroundColor="transparent" /> */}
 
 export default function Product() {
     return(
-        <View>
-            <Text>Olá mundo</Text>
-        </View>
+        <ProductContainer>
+            <HeaderArea>
+                <ProductImage source={require('../../assets/img/product2.jpg')} />
+                
+                <RowButtons>
+                    <BackButton>
+                        <FavoriteIcon color="#333" height={25} width={25} />
+                    </BackButton>
+
+                    <FavoriteButton>
+                        <FavoriteIcon color="#333" height={25} width={25} />
+                    </FavoriteButton>
+                </RowButtons>
+            </HeaderArea>
+
+            <InfoArea>
+                <InfoHeader>
+                    <InfoName>Corduroy Cap</InfoName>
+                    <InfoPrice>R$ 36.00</InfoPrice>
+                </InfoHeader>
+
+                <InfoDescription>
+                    A corduroy cap will perfectly complement your image. This hat is suitable
+                    for both the classic style and for casual style.
+                </InfoDescription>
+
+                <InfoColorSelect>
+                    <InfoColorSelectTile>Color</InfoColorSelectTile>
+
+                    <InfoColorSelectOptions>
+                        <InfoColorOption />
+                        <InfoColorOption />
+                        <InfoColorOption />
+                    </InfoColorSelectOptions>
+                </InfoColorSelect>
+
+                <InfoSizeSelect>
+                    <InfoSizeSelectTitle>Size</InfoSizeSelectTitle>
+
+                    <InfoSizeSelectOptions>
+                        <InfoSizeOption>S</InfoSizeOption>
+                        <InfoSizeOption>M</InfoSizeOption>
+                        <InfoSizeOption>L</InfoSizeOption>
+                    </InfoSizeSelectOptions>
+                </InfoSizeSelect>
+
+                <AddToCartButton>
+                    <CartIcon color="#fff" width={25} height={25} />
+                    <AddToCartText>Add to cart</AddToCartText>
+                </AddToCartButton>
+            </InfoArea>
+        </ProductContainer>
     )
 }
