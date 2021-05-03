ec9cb4e9320d7a5c1b1f6c2218659b8c1f7de7fa
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-03 11:49:48 -0300

    Organizando styles da tela Product

--- src/screens/Product/styles.js
+++ src/screens/Product/styles.js
@@ -12,14 +12,15 @@ export const HeaderArea = styled.ImageBackground`
 export const RowButtons = styled.View``;
 export const RowDefaultButton = styled.TouchableOpacity``;
 
+
 export const InfoArea = styled.View`
+    flex: 1;
+    background: ${white};
+    border: 1px solid ${gray200};
     padding: 30px;
     border-top-left-radius:  50px;
     border-top-right-radius: 50px;
     margin-top: -150px;
-    background: ${white};
-    flex: 1;
-    border: 1px solid ${gray200};
     border-bottom-width: 0;
 `;
 export const InfoHeader = styled.View`
@@ -28,14 +29,14 @@ export const InfoHeader = styled.View`
     align-items: center;
 `;
 export const InfoHeaderText = styled.Text`
+    color: ${gray800};
     font-weight: bold;
     font-size: 17px;
-    color: ${gray800};
 `;
 export const InfoDescription = styled.Text`
+    color: ${gray500};
     margin-top: 15px;
     font-size: 15px;
-    color: ${gray500};
 `;
 export const InfoSelect = styled.View`
     margin-top: 20px;
@@ -50,40 +51,40 @@ export const InfoSelectOptions = styled.View`
     margin-top: 10px;
 `;
 export const InfoOptionForSelection = styled.TouchableOpacity`
+    background: ${props=>props.background || gray100};
+    align-items: center;
+    justify-content: center;
     width: 50px;
     height: 50px;
     border-radius: 25px;
-    background: ${props=>props.background || gray100};
     margin-right: 10px;
-    align-items: center;
-    justify-content: center;
 `;
 export const InfoOptionSelected = styled.View`
-    width: 20px;
-    height: 20px;
-    border-radius: 10px;
     background: #000;
-    border: 2px solid ${white};
     opacity: ${props=>props.opacity};
+    align-items: center;
+    justify-content: center;
     position: absolute;
     top: 0;
     right: 0;
-    align-items: center;
-    justify-content: center;
+    width: 20px;
+    height: 20px;
+    border-radius: 10px;
+    border: 2px solid ${white};
 `;
 export const InfoOptionForSelectionText = styled.Text`
     color: ${props=>props.color || gray800};
-    font-size: 17px;
     font-weight: bold;
+    font-size: 17px;
 `;
 export const AddToCartButton = styled.TouchableOpacity`
     background: #000;
     flex-direction: row;
     align-items: center;
     justify-content: center;
+    margin-top: auto;
     height: 62px;
     border-radius: 10px;
-    margin-top: auto;
 `;
 export const AddToCartText = styled.Text`
     color: ${white};
