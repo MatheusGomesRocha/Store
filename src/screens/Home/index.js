24a4328beeb192aaa3291db76d56bed44d570697
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-05 15:40:05 -0300

    Apagando e recriando totalmente o design da tela home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,160 +1,200 @@
-import React from 'react';
-import { ScrollView, View } from 'react-native';
+import React, {useState} from 'react';
+import { ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 
-import WatchPng from '../../assets/img/watch.png';
 import SearchIcon from '../../assets/svg/search.svg';
-import NotificationIcon from '../../assets/svg/bell.svg';
+import AdjustIcon from '../../assets/svg/adjust.svg';
 import FavoriteIcon from '../../assets/svg/heart.svg';
+import MenuGridIcon from '../../assets/svg/menu-grid.svg';
 
 import {
     HomeContainer,
 
     Header,
-    DivInputSearch,
-    Input,
-    ButtonNotification,
+    HeaderButton,
 
-    PromotionArea,
-    PromotionInsideLeft,
-    PromotionText,
-    PromotionImg,
-
-    FlashSaleArea,
-    FlashSaleHeader,
-    FlashSaleTitle,
-    FlashSaleClosingIn,
-    FlashSaleClosingInText,
-    FlashSaleCount,
-    FlashSaleTime,
-    FlashSaleItem,
-    FlashSaleImgView,
-    FlashSaleImg,
-    FlashSaleName,
-    FlashSalePrice,
-    FlashSaleDiscountView,
-    FlashSaleDiscountText,
+    FilterArea,
+    FilterItem,
+    FilterText,
 
     ProductsArea,
-    ProductsFilter,
-    ProductsFilterText,
-    ProductsArray,
+    ProductsHeader,
+    ProductsHeaderRow,
+    ProductsTitle,
+    ProductsSmallTitle,
+    ProductsSeeAll,
+    ProductsSeeAllText,
     ProductsItem,
+    ProductsImgContainer,
     ProductsImg,
+    ProductsFavoriteButton,
+    ProductsInfo,
     ProductsName,
     ProductsPrice,
-    ProductsFavoriteBtn
 } from './styles';
-import { defaultColor, gray200, gray800 } from '../../styles';
 
+import { defaultColor } from '../../styles';
 
 export default function Home() {
     const productArray = [
-        {id: 1, img: '../../assets/img/watch.png', name: 'Apple Watch', price: 185.41, marginTop: -50},
-        {id: 2, img: '../../assets/img/watch.png', name: 'Apple Watch', price: 185.41},
-        {id: 3, img: '../../assets/img/watch.png', name: 'Apple Watch', price: 185.41, marginTop: -50},
-        {id: 4, img: '../../assets/img/watch.png', name: 'Apple Watch', price: 185.41},
+        {id: 1, img: require('../../assets/img/dress2.png'), name: 'Winter Dress', price: 185.41},
+        {id: 2, img: require('../../assets/img/dress2.png'), name: 'Snicker Air Jordan XXL', price: 90.00},
+        {id: 3, img: require('../../assets/img/dress2.png'), name: 'Apple Watch', price: 110.90},
+        {id: 4, img: require('../../assets/img/dress2.png'), name: 'Cowboy hat', price: 99.90},
     ];
 
+    const [filter, setFilter] = useState('all');
+
     const navigation = useNavigation();
+
+    const FilterComponent = () => {
+        return(
+            <ScrollView horizontal={true}>
+                <FilterArea>
+                    <FilterItem background={filter === 'all' && '#FCEDEA'} onPress={() => setFilter('all')}>
+                        <FilterText color={filter === 'all' && defaultColor}>All</FilterText>
+                    </FilterItem>
+
+                    <FilterItem background={filter === 'huddi' && '#FCEDEA'} onPress={() => setFilter('huddi')}>
+                        <FilterText color={filter === 'huddi' && defaultColor}>Huddi</FilterText>
+                    </FilterItem>
+                            
+                    <FilterItem background={filter === 'jacket' && '#FCEDEA'} onPress={() => setFilter('jacket')}>
+                        <FilterText color={filter === 'jacket' && defaultColor}>Jacket</FilterText>
+                    </FilterItem>
+
+                    <FilterItem background={filter === 'jeans' && '#FCEDEA'} onPress={() => setFilter('jeans')}>
+                        <FilterText color={filter === 'jeans' && defaultColor}>Jeans</FilterText>
+                    </FilterItem>
+
+                    <FilterItem background={filter === 'hat' && '#FCEDEA'} onPress={() => setFilter('hat')}>
+                        <FilterText color={filter === 'hat' && defaultColor}>Hats</FilterText>
+                    </FilterItem>
+
+                    <FilterItem background={filter === 'shirt' && '#FCEDEA'} onPress={() => setFilter('shirt')}>
+                        <FilterText color={filter === 'shirt' && defaultColor}>Shirts</FilterText>
+                    </FilterItem>
+                </FilterArea>
+            </ScrollView>
+        )
+    }
+
+    const RecommendedComponent = () => {
+        return(
+            <ProductsArea>
+                <ProductsHeader>
+                    <ProductsTitle>Recommended for your</ProductsTitle>
+                    <ProductsSmallTitle>Based on search</ProductsSmallTitle>
+                </ProductsHeader>
+
+                <ScrollView horizontal={true}>
+                    {productArray.map((item, k) => (
+                        <ProductsItem key={k}>
+                            <ProductsImgContainer>
+                                <ProductsImg resizeMode="center" source={item.img} />
+                                <ProductsFavoriteButton>
+                                    <FavoriteIcon fill="#fff" width={20} height={20} />
+                                </ProductsFavoriteButton>
+                            </ProductsImgContainer>
+
+                            <ProductsInfo>
+                                <ProductsName numberOfLines={1}>{item.name}</ProductsName>
+                                <ProductsPrice>R${item.price}</ProductsPrice>
+                            </ProductsInfo>
+                        </ProductsItem>
+                    ))}
+                </ScrollView>
+            </ProductsArea>
+        )
+    }
+
+    const TopCollectionComponent = () => {
+        return(
+            <ProductsArea>
+                <ProductsHeaderRow>
+                    <ProductsTitle>Top Collection</ProductsTitle>
+                    <ProductsSeeAll>
+                        <ProductsSeeAllText>See All</ProductsSeeAllText>
+                    </ProductsSeeAll>
+                </ProductsHeaderRow>
+
+                <ScrollView horizontal={true}>
+                    {productArray.map((item, k) => (
+                        <ProductsItem key={k}>
+                            <ProductsImgContainer>
+                                <ProductsImg resizeMode="center" source={item.img} />
+                                <ProductsFavoriteButton>
+                                    <FavoriteIcon fill="#fff" width={20} height={20} />
+                                </ProductsFavoriteButton>
+                            </ProductsImgContainer>
+
+                            <ProductsInfo>
+                                <ProductsName numberOfLines={1}>{item.name}</ProductsName>
+                                <ProductsPrice>R${item.price}</ProductsPrice>
+                            </ProductsInfo>
+                        </ProductsItem>
+                    ))}
+                </ScrollView>
+            </ProductsArea>
+        )
+    }
+
+    const UpcomingComponent = () => {
+        return(
+            <ProductsArea>
+                <ProductsHeaderRow>
+                    <ProductsTitle>Upcoming Clothes</ProductsTitle>
+                    <ProductsSeeAll>
+                        <ProductsSeeAllText>See All</ProductsSeeAllText>
+                    </ProductsSeeAll>
+                </ProductsHeaderRow>
+
+                <ScrollView horizontal={true}>
+                    {productArray.map((item, k) => (
+                        <ProductsItem key={k}>
+                            <ProductsImgContainer>
+                                <ProductsImg resizeMode="center" source={item.img} />
+                                <ProductsFavoriteButton>
+                                    <FavoriteIcon fill="#fff" width={20} height={20} />
+                                </ProductsFavoriteButton>
+                            </ProductsImgContainer>
+
+                            <ProductsInfo>
+                                <ProductsName numberOfLines={1}>{item.name}</ProductsName>
+                                <ProductsPrice>R${item.price}</ProductsPrice>
+                            </ProductsInfo>
+                        </ProductsItem>
+                    ))}
+                </ScrollView>
+            </ProductsArea>
+        )
+    }
 
     return(
         <HomeContainer>
             <ScrollView>
-
                 <Header>
-                    <DivInputSearch>
-                        <SearchIcon fill={gray200} width="25px" height="25px" />
-                        <Input placeholderTextColor={gray200} placeholder="Search" />
-                    </DivInputSearch>
+                    <HeaderButton style={{backgroundColor: '#000'}}>
+                        <MenuGridIcon fill="#fff" width={20} height={20} />
+                    </HeaderButton>
 
-                    <ButtonNotification>
-                        <NotificationIcon fill={gray800} width="25px" height="25px" />
-                    </ButtonNotification>
+                    <HeaderButton style={{marginLeft: 'auto'}}>
+                        <SearchIcon fill="#000" width={20} height={20}/>
+                    </HeaderButton>
+
+                    <HeaderButton>
+                        <AdjustIcon fill="#000" width={20} height={20}/>
+                    </HeaderButton>
                 </Header>
 
-                <PromotionArea>
-                    <PromotionInsideLeft>
-                        <PromotionText>Hot Sale</PromotionText>
-                        <PromotionText style={{fontWeight: 'bold'}}>40% Off</PromotionText>
-                        <PromotionText style={{marginTop: 25, fontSize: 20}}>Rolex Smartwatch</PromotionText>
-                    </PromotionInsideLeft>
+                <FilterComponent />
 
-                    <PromotionImg source={require('../../assets/img/watch.png')} />
-                </PromotionArea>
+                <RecommendedComponent />
 
-                <FlashSaleArea>
-                    <FlashSaleHeader>
-                        <FlashSaleTitle>Flash Sale</FlashSaleTitle>
-                        <FlashSaleClosingIn>
-                            <FlashSaleClosingInText>Closing in:</FlashSaleClosingInText>
+                <TopCollectionComponent />
 
-                            <FlashSaleCount>
-                                <FlashSaleTime>09</FlashSaleTime>
-                            </FlashSaleCount>
-                            <FlashSaleCount>
-                                <FlashSaleTime>27</FlashSaleTime>
-                            </FlashSaleCount>
-                            <FlashSaleCount>
-                                <FlashSaleTime>51</FlashSaleTime>
-                            </FlashSaleCount>
-
-                        </FlashSaleClosingIn>
-                    </FlashSaleHeader>
-                    <ScrollView contentContainerStyle={{marginVertical: 10, paddingHorizontal: 5}} horizontal={true}>
-                        {productArray.map((item, k) => (
-                            <FlashSaleItem onPress={() => navigation.navigate('product')} key={k}>
-                                <FlashSaleImgView>
-                                    <FlashSaleImg source={require('../../assets/img/watch.png')} />
-                                </FlashSaleImgView>
-                                <FlashSaleName>{item.name}</FlashSaleName>
-                                <FlashSalePrice>R$ {item.price}</FlashSalePrice>
-
-                                <FlashSaleDiscountView>
-                                    <FlashSaleDiscountText>50% off</FlashSaleDiscountText>
-                                </FlashSaleDiscountView>
-                            </FlashSaleItem>
-                        ))}
-                    </ScrollView>
-                </FlashSaleArea>
-
-                <ProductsArea>
-                    <ScrollView horizontal={true} contentContainerStyle={{marginVertical: 10, paddingHorizontal: 5}}>
-                            <ProductsFilter style={{backgroundColor: defaultColor}}>
-                                <ProductsFilterText style={{color: '#fff'}}>All</ProductsFilterText>
-                            </ProductsFilter>
-                            <ProductsFilter>
-                                <ProductsFilterText>Top</ProductsFilterText>
-                            </ProductsFilter>
-                            <ProductsFilter>
-                                <ProductsFilterText>Recomended</ProductsFilterText>
-                            </ProductsFilter>
-                            <ProductsFilter>
-                                <ProductsFilterText>Most Viewed</ProductsFilterText>
-                            </ProductsFilter>
-                            <ProductsFilter>
-                                <ProductsFilterText>Low Price</ProductsFilterText>
-                            </ProductsFilter>
-                    </ScrollView>
-
-                    <ProductsArray>
-                        {productArray.map((item, k) => (
-                            <View key={k} style={{margin: 10, width: '45%', marginTop: item.marginTop}}>
-                                    <ProductsItem onPress={() => navigation.navigate('product')} key={k}>
-                                        <ProductsImg source={require('../../assets/img/dress2.png')} />
-
-                                        <ProductsFavoriteBtn>
-                                            <FavoriteIcon fill={gray800} width="20px" height="20px" />
-                                        </ProductsFavoriteBtn>
-                                    </ProductsItem>
-
-                                    <ProductsName>{item.name}</ProductsName>
-                                    <ProductsPrice>R$ {item.price}</ProductsPrice>
-                            </View>
-                        ))}
-                    </ProductsArray>
-                </ProductsArea>
-
+                <UpcomingComponent />
+                
             </ScrollView>
         </HomeContainer>
     )
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -9,184 +9,99 @@ export const HomeContainer = styled.SafeAreaView`
 
 export const Header = styled.View`
     flex-direction: row;
+    margin: 20px 15px 40px 0;
+`;
+export const HeaderButton = styled.TouchableOpacity`
     align-items: center;
-    margin: 20px 10px 10px 10px;
-`;
-export const DivInputSearch = styled.View`
-    flex-direction: row;
-    align-items: center;
-    flex: 1;
-    background: ${gray50};
-    border-radius: 25px;
-    height: 56px;
-    padding-left: 15px;
-`;
-export const Input = styled.TextInput`
-    color: ${gray800};
-    flex: 1;
-    padding-left: 15px;
-`;
-export const ButtonNotification = styled.TouchableOpacity`
-    background: ${gray50};
+    justify-content: center;
+    width: 50px;
+    height: 50px;
     border: 1px solid ${gray100};
+    border-radius: 30px;
+    margin-left: 15px;
+`;
+
+export const FilterArea = styled.View`
+    flex-direction: row;
+    margin: 0 15px 0 5px;
+`;
+export const FilterItem = styled.TouchableOpacity`
+    background: ${props=>props.background || gray100};
     align-items: center;
-    justify-content: center;
-    height: 56px;
-    width: 56px;
-    border-radius: 28px;
+    justify-content: center; 
+    min-width: 60px;
+    height: 40px;
     margin-left: 10px;
-`;
-
-
-export const PromotionArea = styled.View`
-    background: ${defaultColor};
-    flex-direction: row;
-    justify-content: space-between;
-    margin: 10px;
-    border-radius: 20px;
-    height: 180px;
-    padding: 0 5px 0 15px;
-`;
-export const PromotionInsideLeft = styled.View`
-    margin: 20px 0;
-    width: 55%;
-`;
-export const PromotionText = styled.Text`
-    color: ${white};
-    font-size: 25px;
-    margin-top: 1px;
-`;
-export const PromotionImg = styled.Image`
-    height: 180px;
-    width: 180px;
-`;
-
-
-export const FlashSaleArea = styled.View`
-`;
-export const FlashSaleHeader = styled.View`
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    margin: 10px;
-`;
-export const FlashSaleTitle = styled.Text`
-    font-size: 20px;
-`;
-export const FlashSaleClosingIn = styled.View`
-    flex-direction: row;
-`;
-export const FlashSaleClosingInText = styled.Text`
-    color: ${gray200};
-`;
-export const FlashSaleCount = styled.View`
-    background: ${gray100};
-    align-items: center;
-    justify-content: center;
-    width: 25px;
-    height: 25px;
-    border-radius: 3px;
-    margin-left: 5px;
-`;
-export const FlashSaleTime = styled.Text`
-    font-weight: bold;
-`;
-export const FlashSaleItem = styled.TouchableOpacity`
-    background: ${gray50};
-    align-items: center;
-    width: 150px;
-    height: 220px;
-    margin: 0 5px;
     border-radius: 15px;
+    padding: 0 20px;
 `;
-export const FlashSaleImgView = styled.View`
-    align-items: center;
-    justify-content: center;
-    background: ${gray100};
-    width: 90px;
-    height: 90px;
-    border-radius: 50px;
-    margin-top: 55px;
-`;
-export const FlashSaleImg = styled.Image`
-    width: 90px;
-    height: 90px;
-`;
-export const FlashSaleName = styled.Text`
-    text-align: left;
-    margin-top: 10px;
+export const FilterText = styled.Text`
+    color: ${props=>props.color || gray500};
     font-size: 16px;
-    height: 27px;
-    width: 100%;
-    margin-left: 25px;
 `;
-export const FlashSalePrice = styled.Text`
-    font-weight: bold;
-    text-align: left;
-    font-size: 16px;
-    width: 100%;
-    margin-left: 25px;
-`;
-export const FlashSaleDiscountView = styled.View`
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    border: 1px solid ${defaultColor};
-    border-radius: 20px;
-    padding: 3px 5px;
-`;
-export const FlashSaleDiscountText = styled.Text`
-    color: ${defaultColor};
-`;
-
 
 export const ProductsArea = styled.View`
-    margin-top: 25px;
+    margin-top: 30px;
 `;
-export const ProductsFilter = styled.TouchableOpacity`
-    min-width: 70px;
-    height: 50px;
-    background: ${gray100};
-    border-radius: 50px;
-    padding: 0 20px;
-    align-items: center;
-    justify-content: center;
-    margin: 0 5px;
+export const ProductsHeader = styled.View`
+    margin: 0 15px;
 `;
-export const ProductsFilterText = styled.Text`
-    color: ${gray500};
-`;
-export const ProductsArray = styled.View`
+export const ProductsHeaderRow = styled.View`
     flex-direction: row;
-    flex-wrap: wrap;
-    margin-top: 70px;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 15px;
+`;
+export const ProductsTitle = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+`;
+export const ProductsSmallTitle = styled.Text`
+    color: ${gray200};
+`;
+export const ProductsSeeAll = styled.TouchableOpacity``;
+export const ProductsSeeAllText = styled.Text`
+    color: ${gray200};
+    font-size: 16px;
 `;
 export const ProductsItem = styled.TouchableOpacity`
+    margin: 15px;
+`;
+export const ProductsImgContainer = styled.View`
+    background: ${gray50};
+    align-items: center;
+    justify-content: center;
+    width: 200px;
+    height: 220px;
     border-radius: 15px;
-    background: ${gray100};
-    padding: 10px 0;
 `;
 export const ProductsImg = styled.Image`
-    width: 100%;
-    height: 220px;
+    width: 200px;
+    height: 200px;
+`;
+export const ProductsFavoriteButton = styled.TouchableOpacity`
+    background: #000;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+`;
+export const ProductsInfo = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;                        
+    margin-top: 10px;
 `;
 export const ProductsName = styled.Text`
     color: ${gray800};
-    margin-top: 10px;
+    font-size: 16px;
+    width: 120px;
 `;
 export const ProductsPrice = styled.Text`
-    color: ${defaultColor};
     font-weight: bold;
-    font-size: 16px;
-`;
-export const ProductsFavoriteBtn = styled.TouchableOpacity`
-    position: absolute;
-    align-items: center;
-    justify-content: center;
-    background: ${white};
-    top: 15px;
-    right: 15px;
-    width: 40px;
-    height: 40px;
-    border-radius: 20px;
+    color: ${gray800};
 `;
