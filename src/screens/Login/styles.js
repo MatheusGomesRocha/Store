f6f11834ee9aeeda2fe938eed57cf0698c1d1cbe
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-06 09:34:22 -0300

    Criando arquivo styles e todas os Components que irei usar (por enquanto) para a tela de login

---
+++ src/screens/Login/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+
+export const LoginContainer = styled.SafeAreaView``;
+
+export const LogoContainer = styled.View``;
+export const LogoImg = styled.Image``;
+
+export const Title = styled.Text``;
+export const GoToText = styled.Text``;
+export const GoToButton = styled.TouchableOpacity``;
+export const GoToButtonText = styled.Text``;
+
+export const SocialLoginContainer = styled.View``;
+export const SocialLoginButton = styled.TouchableOpacity`
+    background: ${props=>props.background || 'transparent'};
+`;
+
+export const SeparatorLine = styled.View``;
+export const SeparatorText = styled.Text``;
+
+export const InputContainer = styled.View``;
+export const LabelInput = styled.Text``;
+export const Input = styled.TextInput``;
+export const InputViewForPassword = styled.View``;
+
+export const FooterContainer = styled.View``;
+export const RememberMe = styled.View``;
+export const RememberCheckbox = styled.CheckBox``;
+export const RememberText = styled.Text``;
+export const ForgotPasswordButton = styled.TouchableOpacity``;
+export const ForgotPasswordText = styled.Text``;
+
+export const SubmitButton = styled.TouchableOpacity``;
+export const SubmitText = styled.Text``;
