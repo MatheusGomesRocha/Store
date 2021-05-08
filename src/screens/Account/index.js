4fcdbb95c4c868ed74be7f133782e733a30b2196
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-08 16:10:44 -0300

    Criando estrutura e Components da tela Account

--- src/screens/Account/index.js
+++ src/screens/Account/index.js
@@ -1,12 +1,77 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import CheckIcon from '../../assets/svg/check.svg';
+import MaleIcon from '../../assets/svg/male.svg';
+import FemaleIcon from '../../assets/svg/female.svg';
 
 import {
-    AccountContainer
+    AccountContainer,
+
+    AccountConfirmButton,
+
+    AccountTitle,
+
+    AccountItem,
+    AccountItemTitle,
+
+    AccountItemPhotoContainer,
+    AccountItemImgContainer,
+    AccountItemImg,
+    AccountItemImgUpload,
+    
+    AccountItemInput,
+
+    AccountItemGenderButton
 } from './styles';
 
 export default() => {
     return(
         <AccountContainer>
+            <AccountConfirmButton>
+                <CheckIcon fill="#fff" width={15} height={15} />
+            </AccountConfirmButton>
+
+            <AccountTitle>Account</AccountTitle>
+
+            <AccountItem>
+                <AccountItemTitle>Photo</AccountItemTitle>
+
+                <AccountItemPhotoContainer>
+                    <AccountItemImgContainer source={require('../../assets/img/Eu.jpeg')} />
+                    <TouchableOpacity>
+                        <AccountItemImgUpload>Upload Image</AccountItemImgUpload>
+                    </TouchableOpacity>
+                </AccountItemPhotoContainer>
+            </AccountItem>
+
+            
+            <AccountItem>
+                <AccountItemTitle>Name</AccountItemTitle>
+                <AccountItemInput placeholder="Name" />
+            </AccountItem>
+
+            <AccountItem>
+                <AccountItemTitle>Age</AccountItemTitle>
+                <AccountItemInput placeholder="24" />
+            </AccountItem>
+
+            <AccountItem>
+                <AccountItemTitle>Gender</AccountItemTitle>
+
+                <AccountItemGenderButton>
+                    <FemaleIcon fill="#fff" width={15} height={15} />
+                </AccountItemGenderButton>
+
+                <AccountItemGenderButton>
+                    <MaleIcon fill="#fff" width={15} height={15} />
+                </AccountItemGenderButton>
+            </AccountItem>
+
+            <AccountItem>
+                <AccountItemTitle>Email</AccountItemTitle>
+                <AccountItemInput placeholder="matheusgomes192@hotmail.com" />
+            </AccountItem>
             
         </AccountContainer>
     )
--- src/screens/Account/styles.js
+++ src/screens/Account/styles.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components/native';
 
 export const AccountContainer = styled.SafeAreaView``;
+
+export const AccountConfirmButton = styled.TouchableOpacity``;
+
+export const AccountTitle = styled.Text``;
+
+export const AccountItem = styled.View``
+export const AccountItemTitle = styled.Text``;
+
+export const AccountItemPhotoContainer = styled.View``;
+export const AccountItemImgContainer = styled.View``;
+export const AccountItemImg = styled.Image``;
+export const AccountItemImgUpload = styled.Text``;
+
+export const AccountItemInput = styled.TextInput``;
+
+export const AccountItemGenderButton = styled.TouchableOpacity``;
