ef2615812a9bcd3429ad3738cada702b95bc89b4
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-08 10:14:17 -0300

    Finalizando Design da tela de Profile / Settings para o perfil do Usuário

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { ScrollView, Switch } from 'react-native';
 
 import WorldIcon from '../../assets/svg/world.svg';
 import BellIcon from '../../assets/svg/bell.svg';
@@ -20,16 +21,17 @@ import {
     ProfileItemImg,
     ProfileItemInfo,
     ProfileItemTitle,
+    ProfileItemFlexEnd,
     ProfileItemSmallerTitle,
     ProfileItemButton,
 } from './styles';
 
 export default function Home() {
     const settingsArray = [
-        {id: 1, Icon: WorldIcon, blue: true, title: 'Language', smallerTitle: 'English', navigation: true},
-        {id: 2, Icon: BellIcon, orange: true, title: 'Notifications', navigation: true},
-        {id: 3, Icon: MoonIcon, purple: true, title: 'Dark Mode', smallerTitle: 'Off'},
-        {id: 4, Icon: HelpIcon, red: true, title: 'Help', navigation: true},
+        {id: 1, Icon: WorldIcon, title: 'Language', smallerTitle: 'English', navigation: true, background: '#C8F8FF', fill: '#00BDDB'},
+        {id: 2, Icon: BellIcon, title: 'Notifications', navigation: true, background: '#F8E3D3', fill: '#E88164'},
+        {id: 3, Icon: MoonIcon, title: 'Dark Mode', switch: true, background: '#DAD5EB', fill: '#3E51C2'},
+        {id: 4, Icon: HelpIcon, title: 'Help', navigation: true, background: '#F7C7CF', fill: '#E02544'},
     ];
 
     const SettingsComponent = () => {
@@ -39,17 +41,34 @@ export default function Home() {
                 
                 {settingsArray.map((item, k) => (
                     <ProfileItem key={k}>
-                        <ProfileItemImgContainer>
-                            <item.Icon fill="#000" width={20} height={20} />
+                        <ProfileItemImgContainer background={item.background}>
+                            <item.Icon fill={item.fill} width={25} height={25} />
                         </ProfileItemImgContainer>
                         <ProfileItemTitle>{item.title}</ProfileItemTitle>
-                        <ProfileItemSmallerTitle>{item.smallerTitle}</ProfileItemSmallerTitle>
+                        <ProfileItemFlexEnd>
+                            <ProfileItemSmallerTitle style={{marginRight: 15}}>{item.smallerTitle}</ProfileItemSmallerTitle>
+                            {item.navigation &&
+                                <ProfileItemButton>
+                                    <AngleRightIcon fill="#000" width={15} height={15} />
+                                </ProfileItemButton>
+                            }
 
-                        {item.navigation &&
-                            <ProfileItemButton>
-                                <AngleRightIcon fill="#000" width={22} height={22} />
-                            </ProfileItemButton>
-                        }
+                            {item.switch &&
+                                <>
+                                    <ProfileItemSmallerTitle style={{marginRight: 15}}>{isEnabled ? 'On' : 'Off'}</ProfileItemSmallerTitle>
+
+                                    <Switch
+                                        trackColor={{ false: "#afb2b1", true: "#efaba1" }}
+                                        thumbColor={isEnabled ? "#fe705f" : "#fff"}
+                                        ios_backgroundColor="#3e3e3e"
+                                        onValueChange={() => setIsEnabled(!isEnabled)}
+                                        value={isEnabled}
+                                    />
+
+                                </>
+                            }
+                            
+                        </ProfileItemFlexEnd>
                     </ProfileItem>
                 ))}    
             </ProfileItemContainer>
@@ -58,27 +77,33 @@ export default function Home() {
 
     const navigation = useNavigation();
 
+    const [isEnabled, setIsEnabled] = useState(false);
+
     return(
         <ProfileContainer>
-            <ProfileTitle>Settings</ProfileTitle>
+            <ScrollView contentContainerStyle={{paddingVertical: 40}}>
 
-            <ProfileSmallerTitle>Account</ProfileSmallerTitle>
-            <ProfileItem>
-                <ProfileItemImgContainer>
-                    <UserIcon fill="#000" width={20} height={20} />
-                </ProfileItemImgContainer>
-                
-                <ProfileItemInfo>
-                    <ProfileItemTitle>Matheus Gomes</ProfileItemTitle>
-                    <ProfileItemSmallerTitle>Personal Info</ProfileItemSmallerTitle>
-                </ProfileItemInfo>
+                <ProfileTitle>Settings</ProfileTitle>
 
-                <ProfileItemButton>
-                    <AngleRightIcon fill="#000" width={22} height={22} />
-                </ProfileItemButton>
-            </ProfileItem>
+                <ProfileSmallerTitle>Account</ProfileSmallerTitle>
+                <ProfileItem>
+                    <ProfileItemImgContainer>
+                        <ProfileItemImg source={require('../../assets/img/Eu.jpeg')} />
+                    </ProfileItemImgContainer>
+                    
+                    <ProfileItemInfo>
+                        <ProfileItemTitle>Matheus Gomes</ProfileItemTitle>
+                        <ProfileItemSmallerTitle>Personal Info</ProfileItemSmallerTitle>
+                    </ProfileItemInfo>
 
-            <SettingsComponent />
+                    <ProfileItemButton>
+                        <AngleRightIcon fill="#000" width={15} height={15} />
+                    </ProfileItemButton>
+                </ProfileItem>
+
+                <SettingsComponent />
+
+            </ScrollView>
         </ProfileContainer>
     )
 }
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -1,21 +1,69 @@
 import styled from 'styled-components/native';
-import {white, gray100} from '../../styles';
+import {white, gray100, gray200, gray50} from '../../styles';
 
 export const ProfileContainer = styled.SafeAreaView`
     flex: 1;
     background: ${white};
+    padding: 0 20px;
 `;
 
-export const ProfileTitle = styled.Text``;
-export const ProfileSmallerTitle = styled.Text``;
+export const ProfileTitle = styled.Text`
+    font-weight: bold;
+    font-size: 30px;
+    margin-bottom: 50px;
+`;
+export const ProfileSmallerTitle = styled.Text`
+    font-size: 22px;
+    margin-bottom: 15px;
+`;
 
-export const ProfileItemContainer = styled.View``;
-export const ProfileItem = styled.View``;
+export const ProfileItemContainer = styled.View`
+    width: 100%;
+    margin-top: 50px;
+`;
+export const ProfileItem = styled.View`
+    flex-direction: row;
+    align-items: center;
+    border-bottom-color: ${gray50};
+    border-bottom-width: 1px;
+    width: 100%;
+    margin-top: 20px;
+    padding-bottom: 20px;
+`;
 export const ProfileItemImgContainer = styled.View`
     background: ${props=>props.background || gray100};
+    align-items: center;
+    justify-content: center;
+    width: 70px;
+    height: 70px;
+    border-radius: 35px;
+    margin-right: 15px;
 `;
-export const ProfileItemImg = styled.Image``;
-export const ProfileItemInfo = styled.View``;
-export const ProfileItemTitle = styled.Text``;
-export const ProfileItemSmallerTitle = styled.Text``;
-export const ProfileItemButton = styled.TouchableOpacity``;
+export const ProfileItemImg = styled.Image`
+    width: 70px;
+    height: 70px;
+    border-radius: 50px;
+`;
+export const ProfileItemInfo = styled.View`
+`;
+export const ProfileItemTitle = styled.Text`
+    font-size: 18px;
+`;
+export const ProfileItemFlexEnd = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-left: auto;
+`;
+export const ProfileItemSmallerTitle = styled.Text`
+    color: ${gray200};
+    font-size: 16px;
+`;
+export const ProfileItemButton = styled.TouchableOpacity`
+    background: ${gray100};
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    border-radius: 15px;
+    margin-left: auto;
+`;
