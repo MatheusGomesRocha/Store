bc04fd268217491d52599c2529a32b91f9350297
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-09 15:28:59 -0300

    Finalizando design da tela Payment

--- src/screens/Payment/index.js
+++ src/screens/Payment/index.js
@@ -1,14 +1,107 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
 
 import {
-    PaymentContainer
+    PaymentContainer,
+
+    PaymentTitle,
+    PaymentSmallerTitle,
+
+    PaymentItemContainer,
+    PaymentItemTitle,
+    PaymentItemButtonArea,
+    PaymentItemButton,
+    PaymentItemButtonText,
+
+    PaymentItemDetailsArea,
+    PaymentItemInputArea,
+    PaymentItemLabel,
+    PaymentItemInput,
+    PaymentItemInputAreaRow,
+
+    PaymentInfoContainer,
+    PaymentInfoHeader,
+    PaymentInfoTitle,
+    PaymentInfoPrice,
+    PaymentInfoBlock,
+    PaymentInfoBlockLabel,
+    PaymentInfoBlockText,
+    PaymentPayButton,
+    PaymentPayButtonText
 } from './styles';
 
 export default () => {
+    const [payType, setPayType] = useState('card');
+
     return(
         <PaymentContainer>
-            
+            <ScrollView contentContainerStyle={{paddingVertical: 50, paddingHorizontal: 20}}>
+                <PaymentTitle>Confirm order and pay</PaymentTitle>
+                <PaymentSmallerTitle>Please make the payment, after that you can wait to your products</PaymentSmallerTitle>
+
+                <PaymentItemContainer>
+                    <PaymentItemTitle>Payment methods</PaymentItemTitle>
+
+                    <PaymentItemButtonArea>
+                        <PaymentItemButton borderC={payType === 'bank' && '#fe705f'} borderW={payType === 'bank' && '1px'} background={payType === 'bank' && '#F8E3D3'} onPress={() => setPayType('bank')}>
+                            <PaymentItemButtonText>Bank card</PaymentItemButtonText>
+                        </PaymentItemButton>
+
+                        <PaymentItemButton borderC={payType === 'pix' && '#fe705f'} borderW={payType === 'pix' && '1px'} background={payType === 'pix' && '#F8E3D3'} onPress={() => setPayType('pix')}>
+                            <PaymentItemButtonText>PIX</PaymentItemButtonText>
+                        </PaymentItemButton>
+
+                        <PaymentItemButton borderC={payType === 'money' && '#fe705f'} borderW={payType === 'money' && '1px'} background={payType === 'money' && '#F8E3D3'} onPress={() => setPayType('money')}>
+                            <PaymentItemButtonText>Money</PaymentItemButtonText>
+                        </PaymentItemButton>
+                    </PaymentItemButtonArea>
+
+                    <PaymentItemDetailsArea>
+                        <PaymentItemTitle>Payment details</PaymentItemTitle>
+
+                        <PaymentItemInputArea>
+                            <PaymentItemLabel>Cardholder Name</PaymentItemLabel>
+                            <PaymentItemInput defaultValue="Matheus Gomes" />
+                        </PaymentItemInputArea>
+
+                        <PaymentItemInputArea>
+                            <PaymentItemInput placeholderTextColor="#afb2b1" placeholder="Card number" />
+                        </PaymentItemInputArea>
+                        
+                        <PaymentItemInputAreaRow>
+                            <PaymentItemInput style={{width: '48%'}} placeholderTextColor="#afb2b1" placeholder="Date" />
+
+                            <PaymentItemInput style={{width: '48%', marginLeft: '4%'}} placeholderTextColor="#afb2b1" placeholder="CCV" />
+                        </PaymentItemInputAreaRow>
+                    </PaymentItemDetailsArea>
+                </PaymentItemContainer>
+
+                <PaymentInfoContainer>
+                    <PaymentInfoHeader>
+                        <PaymentInfoTitle>You have to pay</PaymentInfoTitle>
+                        <PaymentInfoPrice>147.85 BRL</PaymentInfoPrice>
+                    </PaymentInfoHeader>
+
+                    <PaymentInfoBlock>
+                        <PaymentInfoBlockLabel>Company</PaymentInfoBlockLabel>
+                        <PaymentInfoBlockText>Store</PaymentInfoBlockText>
+                    </PaymentInfoBlock>
+
+                    <PaymentInfoBlock>
+                        <PaymentInfoBlockLabel>Order Number</PaymentInfoBlockLabel>
+                        <PaymentInfoBlockText>1148152</PaymentInfoBlockText>
+                    </PaymentInfoBlock>
+
+                    <PaymentInfoBlock>
+                        <PaymentInfoBlockLabel>Service</PaymentInfoBlockLabel>
+                        <PaymentInfoBlockText>Products Order</PaymentInfoBlockText>
+                    </PaymentInfoBlock>
+                </PaymentInfoContainer>
+
+                <PaymentPayButton>
+                    <PaymentPayButtonText>Pay 147.85 BRL</PaymentPayButtonText>
+                </PaymentPayButton>
+            </ScrollView>
         </PaymentContainer>
     )
 }
--- src/screens/Payment/styles.js
+++ src/screens/Payment/styles.js
@@ -1,4 +1,119 @@
 import styled from 'styled-components/native';
-import { white } from '../../styles';
+import { defaultColor, gray100, gray200, gray50, gray500, white } from '../../styles';
 
-export const PaymentContainer = styled.SafeAreaView``;
+export const PaymentContainer = styled.SafeAreaView`
+    flex: 1;
+    background: ${gray50};
+`;
+export const PaymentTitle = styled.Text`
+    font-weight: bold;
+    font-size: 28px;
+`;
+export const PaymentSmallerTitle = styled.Text`
+    color: ${gray200};
+    font-size: 16px;
+    margin-top: 5px;
+`;
+
+export const PaymentItemContainer = styled.View`
+    background: ${white};
+    width: 100%;
+    border-radius: 10px;
+    margin-top: 50px;
+    padding: 20px;
+`;
+export const PaymentItemTitle = styled.Text`
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 18px;
+`;
+export const PaymentItemButtonArea = styled.View`
+    flex-direction: row;
+    margin-top: 15px;
+`;
+export const PaymentItemButton = styled.TouchableOpacity`
+    background: ${props=>props.background || gray50};
+    border-width: ${props=>props.borderW || 0};
+    border-color: ${props=>props.borderC || 'transparent'};
+    align-items: center;
+    justify-content: center;
+    min-width: 100px;
+    height: 60px;
+    border-radius: 10px;
+    margin-right: 20px;
+    padding: 0 10px;
+`;
+export const PaymentItemButtonText = styled.Text`
+`;
+
+export const PaymentItemDetailsArea = styled.View`
+    margin-top: 30px;
+`;
+export const PaymentItemInputArea = styled.View`
+    margin-top: 15px;
+`;
+export const PaymentItemLabel = styled.Text`
+    color: ${gray200};
+    font-size: 16px;
+`;
+export const PaymentItemInput = styled.TextInput`
+    color: #000;
+    border-bottom-color: ${gray200};
+    border-bottom-width: 1px;
+    font-size: 16px;
+`;
+export const PaymentItemInputAreaRow = styled.View`
+    flex-direction: row;
+    margin-top: 15px;
+`;
+
+export const PaymentInfoContainer = styled.View`
+    background: ${defaultColor};
+    width: 100%;
+    margin-top: 50px;
+    border-radius: 10px;
+`;
+export const PaymentInfoHeader = styled.View`
+    padding: 50px 30px;
+    border-width: 1px;
+    border-color: ${white};
+    border-style: dashed;
+    border-radius: 1px;
+`;
+export const PaymentInfoTitle = styled.Text`
+    color: ${gray100};
+    font-size: 20px;
+`;
+export const PaymentInfoPrice = styled.Text`
+    font-weight: bold;
+    color: ${white};
+    font-size: 25px;
+    margin-top: 5px;
+`;
+export const PaymentInfoBlock = styled.View`
+    padding: 30px;
+`;
+export const PaymentInfoBlockLabel = styled.Text`
+    color: #cecece;
+`;
+export const PaymentInfoBlockText = styled.Text`
+    color: ${white};
+    font-weight: bold;
+    font-size: 18px;
+    margin-top: 5px;
+`;
+
+export const PaymentPayButton = styled.TouchableOpacity`
+    background: ${defaultColor};
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 64px;
+    border-radius: 10px;
+    margin-top: 30px;
+`;
+export const PaymentPayButtonText = styled.Text`
+    color: ${white};
+    font-size: 16px;
+`;
+
